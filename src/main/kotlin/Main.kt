68d952345f94bc6927ade2993b1fefc9c1bf8cd5
Karol Korol <karol.korol@silkycoders.com>
2022-04-03 13:24:02 +0200

    added Scaffold

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -22,33 +20,45 @@ fun App() {
         background = Color.DarkGray
     )
 
+    val modifier = Modifier.fillMaxWidth()
     MaterialTheme(
         colors = darkColors,
     ) {
-        val modifier = Modifier.fillMaxWidth()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-        ) {
-            val stopWatch = remember { StopWatch() }
-            val clock = remember { Clock() }
+        val stopWatch = remember { StopWatch() }
+        val clock = remember { Clock() }
 
-            Column {
-                clockDisplay(
-                    formattedTime = clock.formattedTime,
-                    modifier = modifier
+        Scaffold(
+            modifier = modifier,
+            topBar = {
+                TopAppBar(
+                    backgroundColor = MaterialTheme.colors.primary,
+                    navigationIcon = {
+                        Icon(
+                            painter = painterResource("timer.svg"),
+                            contentDescription = "StopWatch"
+                        )
+                    },
+                    title = { Text("StopWatch") }
                 )
+            },
+            content = {
+                Column {
+                    clockDisplay(
+                        formattedTime = clock.formattedTime,
+                        modifier = modifier
+                    )
 
-                stopWatchDisplay(
-                    timeMeasurement = stopWatch.timeMeasurement,
-                    stopWatchState = stopWatch.stopWatchState,
-                    onStartClick = stopWatch::start,
-                    onPauseClick = stopWatch::pause,
-                    onResetClick = stopWatch::restart,
-                    modifier = modifier
-                )
+                    stopWatchDisplay(
+                        timeMeasurement = stopWatch.timeMeasurement,
+                        stopWatchState = stopWatch.stopWatchState,
+                        onStartClick = stopWatch::start,
+                        onPauseClick = stopWatch::pause,
+                        onResetClick = stopWatch::restart,
+                        modifier = modifier
+                    )
+                }
             }
-        }
+        )
     }
 }
 
